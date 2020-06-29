9ade0d8fd17acee6a32de51d9e64bbd9b7561f08
Brian Be <bebrian458@gmail.com>
2020-06-29 12:24:31 -0700

    added contentful export

---
+++ services/wordpress/export.js
@@ -0,0 +1,30 @@
+const { spawn } = require("child_process");
+require("dotenv").config({
+  path: `.env`,
+});
+
+const exportCurrentContentfulData = async () => {
+  const ls = spawn("contentful", [
+    "space",
+    "export",
+    `--space-id=${process.env.CONTENTFUL_SPACE_ID}`,
+  ]);
+
+  ls.stdout.on("data", (data) => {
+    console.log(`${data}`);
+  });
+
+  ls.stderr.on("data", (data) => {
+    console.log(`stderr: ${data}`);
+  });
+
+  ls.on("error", (error) => {
+    console.log(`error: ${error.message}`);
+  });
+
+  ls.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+};
+
+export { exportCurrentContentfulData };
